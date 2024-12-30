96160ab6aad0bfdbff4d96c83b03e8b653f78718
s-rupert <officialrupesh.s@gmail.com>
2024-12-30 11:09:27 -0500

    feat: added myApp.js and index.html

--- myApp.js
+++ myApp.js
@@ -1,20 +1,36 @@
 var express = require('express');
 var app = express();
+app.use(express.json());
 
-absolutePath=__dirname+'/public/index.html';
-app.get('/',function(req,res){
-    res.sendFile(absolutePath);
+absolutePath = __dirname + '/public/index.html';
+app.get('/', function (req, res) {
+  res.sendFile(absolutePath);
 });
 
-app.get('/data', function(req, res, next) {
-    req.unixTimestamp = new Date().getTime();
-    req.utcString = new Date().toUTCString();
-    next();
-  }, function(req, res) {
-    res.json({"unix":req.unixTimestamp, "utc":req.utcString});
+app.get('/api/:date', (req, res) => {
+  const dateParam = req.params.date;
+  let parsedDate = new Date(dateParam);
+  console.log(parsedDate)
+  if (isNaN(parsedDate)) {
+    parsedDate = new Date(Number(dateParam));
+    console.log(parsedDate)
+    if (isNaN(parsedDate)) {
+      return res.json({ error: "Invalid Date" });
+    }
+  }
+  res.json({
+    unix: parsedDate.getTime(),
+    utc: parsedDate.toUTCString()
   });
+});
 
-
+app.get('/data', function (req, res, next) {
+  req.unixTimestamp = new Date("2015-12-25T00:00:00").getTime();
+  req.utcString = new Date("2015-12-25T00:00:00").toUTCString();
+  next();
+}, function (req, res) {
+  res.json({ "unix": req.unixTimestamp, "utc": req.utcString });
+});
 var listener = app.listen(process.env.PORT || 3000, function () {
-    console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
